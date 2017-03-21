0773b1b520a6b338ae284a69d39018917fffad4b
Javad <blackrouter@mail.ru>
2017-03-21 09:26:49 +0330

    Update AssemblyInfo.cs
    beta edition ready for Windows 10 Creators update

--- DWS/Properties/AssemblyInfo.cs
+++ DWS/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Destroy Windows 10 Spying Rollup Edition")]
+[assembly: AssemblyTitle("Destroy Windows 10 Spying Beta")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("WZT")]
+[assembly: AssemblyCompany("WZT - Zeroboard")]
 [assembly: AssemblyProduct("Destroy Windows 10 Spying")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.722.0")]
-[assembly: AssemblyFileVersion("1.6.722.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
